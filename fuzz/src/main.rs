46c4e9f9774a5b50aeec818f416ced9789aaa95c
ysthakur <2004yst@gmail.com>
2022-12-26 12:28:40 -0500

    Don't unwrap result of parsing

--- fuzz/src/main.rs
+++ fuzz/src/main.rs
@@ -5,9 +5,7 @@ extern crate kdl;
 use kdl::KdlDocument;
 
 fn main() {
-    fuzz!(|data: &[u8]| {
-        if let Ok(s) = std::str::from_utf8(data) {
-            let _doc: KdlDocument = s.parse().expect("Failed to parse KDL document");
-        }
+    fuzz!(|data: &str| {
+        let _ = data.parse::<KdlDocument>();
     });
 }
